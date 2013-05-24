f7266d8ea5641f6d7ac5e11c7fe4b5123838bbd2
Xaver Loppenstedt <xaver@loppenstedt.de>
2013-05-24 10:03:27 +0200

    test files

---
+++ test/3dkamikazeball.js
@@ -0,0 +1,135 @@
+var scene, renderer, camera, animatedObjects = [];
+var doCameraAnimation = true;
+
+var Ball = function () {
+    'use strict';
+
+    this.roll = true;
+
+    this.ball = new THREE.SphereGeometry(50, 64, 32);
+    this.material = new THREE.MeshPhongMaterial({ color : 0xffff00, wireframe : true });
+    this.mesh = new THREE.Mesh(this.ball, this.material);
+    this.mesh.position.y = -400;
+    this.mesh.position.x = 0;
+
+    this.doAnimation = function () {
+        this.mesh.position.y += 1;
+        if (this.mesh.position.y > 500) {
+            this.mesh.position.y = 500;
+        }
+        if (camera.position.y < 400) {
+            this.mesh.rotation.x -= 0.1;
+            camera.position.y += 1;
+        }
+    };
+};
+
+var Ground = function () {
+    'use strict';
+
+    this.ground = new THREE.PlaneGeometry(1000, 1000, 100, 100);
+    this.material = new THREE.MeshPhongMaterial({ color : 0x00ff00, wireframe : true });
+    this.mesh = new THREE.Mesh(this.ground, this.material);
+    this.mesh.position.z = -50;
+//    this.mesh.rotation.x = 90;
+};
+
+var Wall = function (x, y, w) {
+    'use strict';
+
+    this.ground = new THREE.PlaneGeometry(w, 100, 10, 10);
+    this.material = new THREE.MeshPhongMaterial({ color : 0x0000ff, wireframe : true });
+    this.mesh = new THREE.Mesh(this.ground, this.material);
+    this.mesh.position.x = x;
+    this.mesh.position.y = y;
+    this.mesh.position.z = 0;
+    this.mesh.rotation.x = Math.PI / 2;
+};
+
+
+function cameraAnimation() {
+    'use strict';
+
+    if (doCameraAnimation) {
+        camera.rotation.x += Math.PI / 360;
+        camera.position.y = -800 * Math.sin(camera.rotation.x);
+        camera.position.z = 650 * Math.cos(camera.rotation.x);
+        if (camera.rotation.x > (Math.PI / 2)) {
+            doCameraAnimation = false;
+        }
+    }
+}
+
+var KamikazeBall3D = {
+    init : function () {
+        'use strict';
+        var hemisphereLight,
+            ball = new Ball(),
+            ground = new Ground(),
+            wall,
+            walls = [];
+
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        camera.position.x = -0;
+        camera.position.y = -800;
+        camera.position.z = 650;
+        camera.rotation.x = 0;
+//        camera.position.y = -600;
+//        camera.position.z = 0;
+//        camera.rotation.x = Math.PI / 2;
+
+        hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
+        scene.add(hemisphereLight);
+
+        scene.add(ground.mesh);
+        animatedObjects.push(ball);
+        scene.add(ball.mesh);
+
+        wall = new Wall(300, 300, 300);
+        scene.add(wall.mesh);
+        walls.push(wall);
+        wall = new Wall(-300, 400, 100);
+        scene.add(wall.mesh);
+        walls.push(wall);
+        wall = new Wall(-200, -200, 200);
+        scene.add(wall.mesh);
+        walls.push(wall);
+        wall = new Wall(-100, 100, 200);
+        scene.add(wall.mesh);
+        walls.push(wall);
+        wall = new Wall(300, -100, 400);
+        scene.add(wall.mesh);
+        walls.push(wall);
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+    },
+
+    animate : function () {
+        'use strict';
+        var i, o;
+
+        requestAnimationFrame(KamikazeBall3D.animate);
+
+        cameraAnimation();
+        for (i = 0; i < animatedObjects.length; i = i + 1) {
+            o = animatedObjects[i];
+            if (o.hasOwnProperty('doAnimation')) {
+                o.doAnimation();
+            }
+        }
+        renderer.render(scene, camera);
+    }
+};
+
+function ready() {
+    'use strict';
+
+    KamikazeBall3D.init();
+    KamikazeBall3D.animate();
+}
+
+$(document).ready(ready());
